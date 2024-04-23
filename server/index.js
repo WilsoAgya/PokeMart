4c67c4fca8b80af46585fd7b72914dedd023a126
Wilson Agyapong <wilsonagyapong16@gmail.com>
2024-04-22 19:49:16 -0700

    initial commit

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const router = require('./route/router')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSucessStatus: 200
+}
+
+app.use(cors(corsOptions));
+app.use('/',router)
+
+
+
+const port = 8080
+const server = app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
